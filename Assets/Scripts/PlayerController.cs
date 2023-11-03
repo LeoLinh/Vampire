accbe4a069866b8b073c7642625f233d9cfa09d4
LeoLinh <nnhatlinhh@gmail.com>
2023-11-03 23:59:41 +0700

    Vampire
    Vampire

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(0f,0f,0f);
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        //Debug.Log(moveInput);
+        moveInput.Normalize();
+        transform.position += moveInput * moveSpeed * Time.deltaTime;
+
+        if (moveInput != Vector3.zero)
+        {
+            anim.SetBool("IsMoving", true);
+        }
+        else
+        {
+            anim.SetBool("IsMoving", false);
+        }
+    }
+}
